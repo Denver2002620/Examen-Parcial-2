b4342d363c66b9436f145ec07a70398f7acb85d9
Denver2002620 <abelg@myuax.com>
2024-11-05 11:58:00 +0100

    x

--- ejercicio4/ejercicio4.cpp
+++ ejercicio4/ejercicio4.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
+#include <algorithm>
 #include "ejercicio4.h"
 int main ()
     {
-
+  int a,b,c;//declarar los variables delos articulos
+  int v=0;//en el que vamos a poner el precio total
+  //luego vamos a pedir al utilizador de poner los precios un por uno
+std::cout<<"pon el precio del primer articulo";
+std::cin>>a;
+std::cout<<"pon el precio del segundo articulo";
+std::cin>>b;
+std::cout<<"pon el precio del tercero articulo";
+std::cin>>c;
+ v   =calcularTotal(a,b,c);//llamar function para calcular el precio total y ponerlo en un variable v
     }
--- ejercicio4/ejercicio4.h
+++ ejercicio4/ejercicio4.h
@@ -7,9 +7,16 @@
 
 
 
-class ejercicio4 {
+    double calcularTotal(double precio1, double precio2, double precio3) {
+        // Crear un arreglo con los precios
+        double precios[3] = {precio1, precio2, precio3};
 
-};
+        // Ordenar los precios de menor a mayor
+        sort(precios, precios + 3);
+
+        // Calcular el total (suma de los dos precios más altos)
+        return precios[1] + precios[2];
+
 
 
 
