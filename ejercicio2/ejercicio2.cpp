7716bd9439e07c163f493ccea7f6a5aa6935be2b
Denver2002620 <abelg@myuax.com>
2024-11-05 11:13:25 +0100

    x

---
+++ ejercicio2/ejercicio2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "ejercicio2.h"
+int ejercicio2()
+    {
+  int a;//declarar el variable
+  std::cout<<"ingressa un numero de 1 a 7 porfavor";//pedir al utilizador de ingressar un numero
+  std::cin>>a;//leerlo
+  semana(a);//llamar la function para determinar el dia de la semana o escribir no es valido si no es un intero de 1 a 7
+  
+    }
+
---
+++ ejercicio2/ejercicio2.h
@@ -0,0 +1,39 @@
+//
+// Created by Usuario on 11/5/2024.
+//
+
+#ifndef EJERCICIO2_H
+#define EJERCICIO2_H
+
+
+int semana(int x)
+{
+//vamos a ir caso por caso
+switch(x)
+{
+  case 1:
+    std::cout<<"lunes";
+    break;
+case 2:
+    std::cout<<"martes";
+    break;
+case 3:
+    std::cout<<"miercoles";
+    break;
+case 4:
+    std::cout<<"jueves";
+    break;
+case 5:
+    std::cout<<"viernes";
+    break;
+case 6:
+    std::cout<<"sabado";
+    break;
+case 7:
+    std::cout<<"domingo";
+    break;
+    default: std::cout<<"el numero no es valido";
+
+
+
+#endif //EJERCICIO2_H
