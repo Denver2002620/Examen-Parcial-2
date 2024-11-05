503f3764273ddadd72e5dd409cc918d1a42faf2c
Denver2002620 <abelg@myuax.com>
2024-11-05 10:42:16 +0100

    added main.cpp and main.h and cmakelist

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Usuario on 11/5/2024.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by Usuario on 11/5/2024.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
